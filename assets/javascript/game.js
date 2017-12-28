e23068a9cd71dccfd5c956f50568bff146c9af32
Joe <jdifranco21@gmail.com>
2017-12-27 18:32:20 -0600

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,71 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   var targetNumber = Math.floor((Math.random() *101) +19);
   $("#targetNumber").text(targetNumber);
 
-  // var counter = 0;
+  var counter = 0;
+  $("#score").text(counter);
 
-  // // Now for the hard part. Creating multiple crystals each with their own unique number value.
+  var win = 0;
+  var loss = 0;
+  $("#winsLosses").html("<br> Wins: <br>" + win + " <br> Losses: <br>" + loss);
 
-  // // We begin by expanding our array to include four options.
-  // var numberOptions = [10, 5, 3, 7];
+  var numberArray = [];
 
-  // // Next we create a for loop to create crystals for every numberOption.
-  // for (var i = 0; i < numberOptions.length; i++) {
+  function randomNumberFuntion() {
+    var i = 0;
+    while (i < 4){
+      var randomNumber = Math.floor((Math.random() *11) + 1);
+      if(numberArray.indexOf(randomNumber) == -1) {
+        numberArray.push(randomNumber);
+        i++;
+      }
+    }
+  }
 
-  //   // For each iteration, we will create an imageCrystal
-  //   var imageCrystal = $("<img>");
+  randomNumberFuntion();
 
-  //   // First each crystal will be given the class ".crystal-image".
-  //   // This will allow the CSS to take effect.
-  //   imageCrystal.addClass("crystal-image");
+  for (var i = 0; i < numberArray.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.attr("src", "assets/images/Crystal.jpg");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("data-crystalvalue", numberArray[i]);
+    $(".crystals").append(imageCrystal);
+  }
 
-  //   // Each imageCrystal will be given a src link to the crystal image
-  //   imageCrystal.attr("src", "https://cs2.pikabu.ru/post_img2/2014/01/19/8/1390135206_1659254356.jpg");
+  function reset() {
+    randomNumberFuntion()
+    for (var i = 0; i < numberArray.length; i++){
+      imageCrystal.attr("data-crystalvalue", numberArray[i])
+    }
 
-  //   // Each imageCrystal will be given a data attribute called data-crystalValue.
-  //   // This data attribute will be set equal to the array value.
-  //   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    targetNumber = Math.floor((Math.random() *101) +19);
+    $("#targetNumber").text(targetNumber);
 
-  //   // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-  //   $("#crystals").append(imageCrystal);
-  // }
+    counter = 0;
+    $("#score").text(counter);
 
-  // // This time, our click event applies to every single crystal on the page. Not just one.
-  // $(".crystal-image").on("click", function() {
+  }
 
-  //   // Determining the crystal's value requires us to extract the value from the data attribute.
-  //   // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-  //   // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-  //   // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+  $(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+    $("#score").text(counter);
 
-  //   var crystalValue = ($(this).attr("data-crystalvalue"));
-  //   crystalValue = parseInt(crystalValue);
-  //   // We then add the crystalValue to the user's "counter" which is a global variable.
-  //   // Every click, from every crystal adds to the global counter.
-  //   counter += crystalValue;
+    if (counter === targetNumber) {
+      alert("Winner");
+      win++;
+      $("#winsLosses").html("<br> Wins: <br>" + win + " <br> Losses: <br>" + loss);
+      reset()
+    }
 
-  //   // All of the same game win-lose logic applies. So the rest remains unchanged.
-  //   alert("New score: " + counter);
-
-  //   if (counter === targetNumber) {
-  //     alert("You win!");
-  //   }
-
-  //   else if (counter >= targetNumber) {
-  //     alert("You lose!!");
-  //   }
-
+    else if (counter >= targetNumber) {
+      alert ("Le Looser");
+      loss++;
+      $("#winsLosses").html("<br> Wins: <br>" + win + " <br> Losses: <br>" + loss);
+      reset()
+    }
   });
+
+});
